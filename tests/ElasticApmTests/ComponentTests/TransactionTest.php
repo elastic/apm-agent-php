b238b4327b7a38cdb8e1066847c83bf5efcbecc4
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2020-09-21 09:31:30 +0300

    Reformat

--- tests/ElasticApmTests/ComponentTests/TransactionTest.php
+++ tests/ElasticApmTests/ComponentTests/TransactionTest.php
@@ -12,6 +12,11 @@ use Elastic\Apm\TransactionInterface;
 
 final class TransactionTest extends ComponentTestCaseBase
 {
+    public static function appCodeForTransactionWithoutSpans(): void
+    {
+        usleep(/* microseconds - 200 milliseconds */ 200 * 1000);
+    }
+
     public function testTransactionWithoutSpans(): void
     {
         $this->sendRequestToInstrumentedAppAndVerifyDataFromAgent(
@@ -24,11 +29,6 @@ final class TransactionTest extends ComponentTestCaseBase
         );
     }
 
-    public static function appCodeForTransactionWithoutSpans(): void
-    {
-        usleep(/* microseconds - 200 milliseconds */ 200 * 1000);
-    }
-
     private function verifyTransactionWithoutSpans(DataFromAgent $dataFromAgent): TransactionInterface
     {
         $this->assertEmpty($dataFromAgent->idToSpan());
@@ -38,6 +38,19 @@ final class TransactionTest extends ComponentTestCaseBase
         $this->assertSame(0, $tx->getDroppedSpansCount());
         $this->assertNull($tx->getParentId());
         return $tx;
+    }
+
+    public static function appCodeForTransactionWithoutSpansCustomProperties(): void
+    {
+        ElasticApm::getCurrentTransaction()->setName('custom TX name');
+        ElasticApm::getCurrentTransaction()->setType('custom TX type');
+        ElasticApm::getCurrentTransaction()->context()->setLabel('string_label_key', 'string_label_value');
+        ElasticApm::getCurrentTransaction()->context()->setLabel('bool_label_key', true);
+        ElasticApm::getCurrentTransaction()->context()->setLabel('int_label_key', -987654321);
+        ElasticApm::getCurrentTransaction()->context()->setLabel('float_label_key', 1234.56789);
+        ElasticApm::getCurrentTransaction()->context()->setLabel('null_label_key', null);
+        usleep(/* microseconds - 200 milliseconds */ 200 * 1000);
+        ElasticApm::getCurrentTransaction()->end(/* milliseconds */ 100);
     }
 
     public function testTransactionWithoutSpansCustomProperties(): void
@@ -56,18 +69,5 @@ final class TransactionTest extends ComponentTestCaseBase
                 $this->assertEquals(100, $tx->getDuration());
             }
         );
-    }
-
-    public static function appCodeForTransactionWithoutSpansCustomProperties(): void
-    {
-        ElasticApm::getCurrentTransaction()->setName('custom TX name');
-        ElasticApm::getCurrentTransaction()->setType('custom TX type');
-        ElasticApm::getCurrentTransaction()->context()->setLabel('string_label_key', 'string_label_value');
-        ElasticApm::getCurrentTransaction()->context()->setLabel('bool_label_key', true);
-        ElasticApm::getCurrentTransaction()->context()->setLabel('int_label_key', -987654321);
-        ElasticApm::getCurrentTransaction()->context()->setLabel('float_label_key', 1234.56789);
-        ElasticApm::getCurrentTransaction()->context()->setLabel('null_label_key', null);
-        usleep(/* microseconds - 200 milliseconds */ 200 * 1000);
-        ElasticApm::getCurrentTransaction()->end(/* milliseconds */ 100);
     }
 }
