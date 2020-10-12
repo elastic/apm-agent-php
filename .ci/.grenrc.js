80d8531249acf6507cbdcc82c93874efe0fc02a8
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-10-12 13:41:10 +0100

    Cosmetic changes

--- .ci/.grenrc.js
+++ .ci/.grenrc.js
@@ -3,16 +3,16 @@ module.exports = {
     "repo": "apm-agent-php",
     "dataSource": "prs",
     "groupBy": {
-        "Breaking changes": ["breaking", "automation"],
-        "Bug Fixes": ["bug", "fix"],
-        "Features": ["enhancement", "internal", "feature", "feat", "documentation"]
+        "Breaking changes": ["breaking"],
+        "Bug fixes": ["bug", "fix"],
+        "Features": ["enhancement", "internal", "feature", "feat"]
     },
     "template": {
         issue: function (placeholders) {
-          return '* ' + placeholders.name + '{pull}' + placeholders.text.replace("#", "") + '[' + placeholders.text + ']';
+          return '* ' + placeholders.name + ': {pull}' + placeholders.text.replace("#", "") + '[' + placeholders.text + ']';
         },
         changelogTitle: "",
-        release: "[[release-notes-{{release}}]]\n==== {{release}} - {{date}}\n{{body}}",
+        release: "[[release-notes-{{release}}]]\n==== {{release}}\n{{body}}",
         releaseSeparator: "",
         group: function (placeholders) {
           return '\n[float]\n===== ' + placeholders.heading;
