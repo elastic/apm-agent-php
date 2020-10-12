607198122158971d7b858f811aace9873f400db5
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2020-10-12 18:10:53 +0300

    Added capturing syslog for 'Package Test' CI pipeline stage to debug failed tests

--- packaging/test/alpine/entrypoint.sh
+++ packaging/test/alpine/entrypoint.sh
@@ -31,4 +31,10 @@ fi
 
 ## Validate the installation works as expected with composer
 composer install
-composer run-script run_component_tests
+set +x
+/usr/sbin/rsyslogd || true
+if ! composer run-script run_component_tests ; then
+    echo 'Something bad happened when running the tests, see the output from the syslog'
+    cat /var/log/syslog
+    exit 1
+fi
