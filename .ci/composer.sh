6a4c2536ea9ef37d0fc97afe14d105b240f3c834
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2020-10-13 09:26:04 +0300

    Changed Docker entry point script to fail if syslog failed to start

--- .ci/composer.sh
+++ .ci/composer.sh
@@ -7,10 +7,10 @@ echo 'extension=elastic_apm.so' > ${PHP_INI}
 echo 'elastic_apm.bootstrap_php_part_file=/app/src/bootstrap_php_part.php' >> ${PHP_INI}
 php -m
 cd /app
-composer install
 
-set +x
-/usr/sbin/rsyslogd || true
+## Validate the installation works as expected with composer
+composer install
+/usr/sbin/rsyslogd
 if ! composer run-script run_component_tests ; then
     echo 'Something bad happened when running the tests, see the output from the syslog'
     cat /var/log/syslog
