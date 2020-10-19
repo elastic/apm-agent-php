a0e32de4ffa84b818bd9e79c05e39a4a6a713761
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2020-10-19 08:53:42 +0300

    Fixed issues with syslog for docker containers used for testing

--- .ci/composer.sh
+++ .ci/composer.sh
@@ -7,12 +7,43 @@ echo 'extension=elastic_apm.so' > ${PHP_INI}
 echo 'elastic_apm.bootstrap_php_part_file=/app/src/bootstrap_php_part.php' >> ${PHP_INI}
 php -m
 cd /app
+
+##
+## Validate the installation works as expected with composer
+##
+
+# Install 3rd party dependencies
 composer install
 
-set +x
-/usr/sbin/rsyslogd || true
+# Start syslog
+
+if which syslogd; then
+    syslogd
+else
+    if which rsyslogd; then
+        rsyslogd
+    else
+        echo 'syslog is not installed'
+        exit 1
+    fi
+fi
+
+
+# Run component tests
+
 if ! composer run-script run_component_tests ; then
     echo 'Something bad happened when running the tests, see the output from the syslog'
-    cat /var/log/syslog
+
+    if [ -f "/var/log/syslog" ]; then
+        cat "/var/log/syslog"
+    else
+        if [ -f "/var/log/messages" ]; then
+        cat "/var/log/messages"
+        else
+            echo 'syslog log file not found'
+            exit 1
+        fi
+    fi
+
     exit 1
 fi
