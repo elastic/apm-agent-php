9b17f1e4a91bbe7fd25f3372494b9c98ff567a6d
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-10-20 15:21:08 +0100

    Fail if agent was not uninstalled

--- packaging/test/centos/entrypoint.sh
+++ packaging/test/centos/entrypoint.sh
@@ -70,6 +70,7 @@ set -x
 if [ "${TYPE}" == "rpm-uninstall" ] ; then
     rpm -e "${PACKAGE}"
     ## Verify if the elastic php agent has been uninstalled
+    php -m > /dev/null 2>&1
     if php -m | grep -q 'elastic' ; then
         echo 'Extension has not been uninstalled.'
         exit 1
