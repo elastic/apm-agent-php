2923db115fc63e269d77dddba30e4376880f3b09
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-10-22 18:58:58 +0100

    Add DOCKERFILE and PHP_VERSION

--- .ci/loop.sh
+++ .ci/loop.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 LOOPS=${1:-50}
+DOCKERFILE=${2:-Dockerfile}
+PHP_VERSION=${3:-7.2}
 for (( c=1; c<=LOOPS; c++ ))
 do  
     echo "Loop $c"
-    make -f .ci/Makefile test
-    make -f .ci/Makefile composer
+    PHP_VERSION=${PHP_VERSION} DOCKERFILE=${DOCKERFILE} make -f .ci/Makefile test
+    PHP_VERSION=${PHP_VERSION} DOCKERFILE=${DOCKERFILE} make -f .ci/Makefile composer
+
+    ## TODO: rename test report files to include the iteration name.
 done
