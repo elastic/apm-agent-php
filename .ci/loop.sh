991542d976ddad1170e49a18911093ffc7d26ff7
Victor Martinez <victormartinezrubio@gmail.com>
2020-10-26 16:00:39 +0000

    [CI] increase number of loops and store tests (#250)

--- .ci/loop.sh
+++ .ci/loop.sh
@@ -3,11 +3,27 @@
 LOOPS=${1:-50}
 DOCKERFILE=${2:-Dockerfile}
 PHP_VERSION=${3:-7.2}
+
+OUTPUT_FOLDER="build/loop-$DOCKERFILE-$PHP_VERSION"
+TEST_REPORT_TEST="junit.xml"
+TEST_REPORT_COMPOSER="_GENERATED/COMPONENT_TESTS/log_as_junit.xml"
+mkdir -p "${OUTPUT_FOLDER}" || true
+
 for (( c=1; c<=LOOPS; c++ ))
 do  
     echo "Loop $c"
-    PHP_VERSION=${PHP_VERSION} DOCKERFILE=${DOCKERFILE} make -f .ci/Makefile test
-    PHP_VERSION=${PHP_VERSION} DOCKERFILE=${DOCKERFILE} make -f .ci/Makefile composer
+    OUTPUT_FILE="$OUTPUT_FOLDER/$c.txt"
+    PHP_VERSION=${PHP_VERSION} DOCKERFILE=${DOCKERFILE} make -f .ci/Makefile test | tee "$OUTPUT_FILE"
+    PHP_VERSION=${PHP_VERSION} DOCKERFILE=${DOCKERFILE} make -f .ci/Makefile composer | tee -a "$OUTPUT_FILE"
 
-    ## TODO: rename test report files to include the iteration name.
+    ## Store test results with the iteration
+    if [ -e $TEST_REPORT_TEST ] ; then
+        mv $TEST_REPORT_TEST "${OUTPUT_FOLDER}/junit-test-$c.xml"
+    fi
+
+    ## Store composer test results with the iteration
+    if [ -e $TEST_REPORT_COMPOSER ] ; then
+        ## Let's copy since mv does not work when reusing the workspace for some required permissions
+        cp $TEST_REPORT_COMPOSER "${OUTPUT_FOLDER}/junit-composer-$c.xml"
+    fi
 done
