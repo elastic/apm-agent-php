b697777c79790b1ac3cf238f912aea3c2eb84373
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-10-26 16:24:08 +0000

    [CI] skip on a PR basis

--- .ci/loop.groovy
+++ .ci/loop.groovy
@@ -3,7 +3,7 @@
 @Library('apm@current') _
 
 pipeline {
-  agent { label 'linux && docker && ubuntu-18.04 && immutable' }
+  agent none
   environment {
     REPO = 'apm-agent-php'
     BASE_DIR = "src/go.elastic.co/apm/${env.REPO}"
@@ -22,70 +22,93 @@ pipeline {
   }
   triggers {
     issueCommentTrigger('(?i).*jenkins\\W+run\\W+(?:the\\W+)?loop\\W+tests(?:\\W+please)?.*')
+    // disable upstream trigger on a PR basis
+    upstream("apm-agent-php/apm-agent-php-mbp${ env.JOB_BASE_NAME.startsWith('PR-') ? 'none' : env.JOB_BASE_NAME }")
   }
   parameters {
     string(name: 'LOOPS', defaultValue: '200', description: 'How many test loops?')
   }
   stages {
-    stage('Checkout') {
-      steps {
-        pipelineManager([ cancelPreviousRunningBuilds: [ when: 'PR' ] ])
-        deleteDir()
-        gitCheckout(basedir: "${BASE_DIR}", githubNotifyFirstTimeContributor: true)
-        stash allowEmpty: true, name: 'source', useDefaultExcludes: false
-      }
-    }
-    stage('BuildAndTest') {
-      failFast false
-      matrix {
-        agent { label 'linux && docker && ubuntu-18.04 && immutable' }
-        options { skipDefaultCheckout() }
-        axes {
-          axis {
-            name 'PHP_VERSION'
-            values '7.2', '7.3', '7.4'
-          }
-          axis {
-            name 'DOCKERFILE'
-            values 'Dockerfile', 'Dockerfile.alpine'
+    stage('Filter build') {
+      agent { label 'linux && docker && ubuntu-18.04 && immutable' }
+      when {
+        beforeAgent true
+        anyOf {
+          triggeredBy cause: "IssueCommentCause"
+          expression {
+            def ret = isUserTrigger() || isUpstreamTrigger()
+            if(!ret){
+              currentBuild.result = 'NOT_BUILT'
+              currentBuild.description = "The build has been skipped"
+              currentBuild.displayName = "#${BUILD_NUMBER}-(Skipped)"
+              echo("the build has been skipped due the trigger is a branch scan and the allow ones are manual, GitHub comment, and upstream job")
+            }
+            return ret
           }
         }
-        stages {
-          stage('Build') {
-            steps {
-              deleteDir()
-              unstash 'source'
-              dir("${BASE_DIR}"){
-                // When running in the CI with multiple parallel stages
-                // the access could be considered as a DDOS attack.
-                retryWithSleep(retries: 3, seconds: 5, backoff: true) {
-                  sh script: "PHP_VERSION=${PHP_VERSION} DOCKERFILE=${DOCKERFILE} make -f .ci/Makefile prepare", label: 'prepare docker image'
+      }
+      stages {
+        stage('Checkout') {
+          steps {
+            pipelineManager([ cancelPreviousRunningBuilds: [ when: 'PR' ] ])
+            deleteDir()
+            gitCheckout(basedir: "${BASE_DIR}", githubNotifyFirstTimeContributor: true)
+            stash allowEmpty: true, name: 'source', useDefaultExcludes: false
+          }
+        }
+        stage('BuildAndTest') {
+          failFast false
+          matrix {
+            agent { label 'linux && docker && ubuntu-18.04 && immutable' }
+            options { skipDefaultCheckout() }
+            axes {
+              axis {
+                name 'PHP_VERSION'
+                values '7.2', '7.3', '7.4'
+              }
+              axis {
+                name 'DOCKERFILE'
+                values 'Dockerfile', 'Dockerfile.alpine'
+              }
+            }
+            stages {
+              stage('Build') {
+                steps {
+                  deleteDir()
+                  unstash 'source'
+                  dir("${BASE_DIR}"){
+                    // When running in the CI with multiple parallel stages
+                    // the access could be considered as a DDOS attack.
+                    retryWithSleep(retries: 3, seconds: 5, backoff: true) {
+                      sh script: "PHP_VERSION=${PHP_VERSION} DOCKERFILE=${DOCKERFILE} make -f .ci/Makefile prepare", label: 'prepare docker image'
+                    }
+                    sh script: "PHP_VERSION=${PHP_VERSION} DOCKERFILE=${DOCKERFILE} make -f .ci/Makefile build", label: 'build'
+                  }
                 }
-                sh script: "PHP_VERSION=${PHP_VERSION} DOCKERFILE=${DOCKERFILE} make -f .ci/Makefile build", label: 'build'
               }
-            }
-          }
-          stage('Tests loop') {
-            steps {
-              dir("${BASE_DIR}"){
-                sh script: "LOOPS=${LOOPS} PHP_VERSION=${PHP_VERSION} DOCKERFILE=${DOCKERFILE} make -f .ci/Makefile loop", label: 'test'
-              }
-            }
-            post {
-              always {
-                junit(allowEmptyResults: true, keepLongStdio: true, testResults: "${BASE_DIR}/build/**/junit*.xml")
-                archiveArtifacts(allowEmptyArchive: true, artifacts: "${BASE_DIR}/build/**/*.txt")
+              stage('Tests loop') {
+                steps {
+                  dir("${BASE_DIR}"){
+                    sh script: "LOOPS=${LOOPS} PHP_VERSION=${PHP_VERSION} DOCKERFILE=${DOCKERFILE} make -f .ci/Makefile loop", label: 'test'
+                  }
+                }
+                post {
+                  always {
+                    junit(allowEmptyResults: true, keepLongStdio: true, testResults: "${BASE_DIR}/build/**/junit*.xml")
+                    archiveArtifacts(allowEmptyArchive: true, artifacts: "${BASE_DIR}/build/**/*.txt")
+                  }
+                }
               }
             }
           }
         }
       }
-    }
-  }
-  post {
-    cleanup {
-      // PR comment is not needed with this pipeline
-      notifyBuildResult(prComment: false)
+      post {
+        cleanup {
+          // PR comment is not needed with this pipeline
+          notifyBuildResult(prComment: false)
+        }
+      }
     }
   }
 }
