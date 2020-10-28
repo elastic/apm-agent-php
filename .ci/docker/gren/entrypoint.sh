6d0acda3417183fe19293095775edb3f141a0151
Victor Martinez <victormartinezrubio@gmail.com>
2020-10-28 13:20:35 +0000

    [CI] changelog automation (#254)

---
+++ .ci/docker/gren/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -exo pipefail
+
+GENERATED_RELEASE_FILE=build/CHANGELOG-RELEASE.asciidoc
+GENERATED_CHANGELOG=build/CHANGELOG.asciidoc
+CHANGELOG=CHANGELOG.asciidoc
+
+# Where the files will be generated
+mkdir -p build || true
+
+/usr/local/bin/gren changelog \
+        --token="${GITHUB_TOKEN}" \
+        --tags="current..${PREVIOUS_TAG}" \
+        --generate \
+        --override \
+        --config .ci/.grenrc.js \
+        --changelog-filename="${GENERATED_RELEASE_FILE}"
+
+echo 'Aggregate generated release notes'
+previousReleasesLine=$(grep -n -m 1 "CHANGELOG_AUTOMATION_KEYWORD" ${CHANGELOG} | cut -f1 -d:)
+{
+  sed '/CHANGELOG_AUTOMATION_KEYWORD/q' ${CHANGELOG}
+  echo '' 
+  sed "s/${PREVIOUS_TAG}/${TAG_NAME}/g" "${GENERATED_RELEASE_FILE}"
+  tail -n +$(( previousReleasesLine + 1 )) ${CHANGELOG}
+} > ${GENERATED_CHANGELOG}
+cp ${GENERATED_CHANGELOG} ${CHANGELOG}
