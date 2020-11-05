9e2f58c14a718612ae3f7a91541e8d155a79562e
Victor Martinez <victormartinezrubio@gmail.com>
2020-11-05 15:25:58 +0000

    [CI] run cmocka as part of the static-check-unit-test stage (#265)

--- .ci/static-check-unit-test.sh
+++ .ci/static-check-unit-test.sh
@@ -1,8 +1,35 @@
 #!/usr/bin/env bash
 set -xe
 
+## Location for the generated test report files
+BUILD_FOLDER=/app/build
+mkdir -p ${BUILD_FOLDER}
+
 ## This make runs PHPT
 make test
+
+## Run cmocka tests
+cd /app/src/ext/unit_tests
+cmake .
+make
+set +e
+make test
+
+## Save errorlevel to be reported later on
+ret=$?
+
+## Manipulate JUnit report without multiple testsuites entries.
+for file in "${BUILD_FOLDER}"/*-unit-tests-junit.xml; do
+    sed -i.bck ':begin;$!N;s#</testsuites>\n<testsuites>##;tbegin;P;D' "${file}"
+done
+
+## Return the error if any
+if [ $ret -ne 0 ] ; then
+    exit 1
+fi
+
+## Enable the error again
+set -e
 
 cd /app
 
