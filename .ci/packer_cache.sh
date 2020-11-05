4da6ca68de2b308fbdbf0f8de8fec76c9cca2cdb
Victor Martinez <victormartinezrubio@gmail.com>
2020-11-05 19:13:17 +0000

    [CI] cache docker images (#270)

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -15,4 +15,11 @@ if [ -x "$(command -v docker)" ]; then
   do
   (retry 2 docker pull "${di}") || echo "Error pulling ${di} Docker image, we continue"
   done
+
+  for version in 7.2 7.3 7.4
+  do
+    PHP_VERSION=${version} make -f .ci/Makefile prepare || true
+    DOCKERFILE=Dockerfile.alpine PHP_VERSION=${version} make -f .ci/Makefile prepare || true
+    PHP_VERSION=${version} make -C packaging prepare || true
+  done
 fi
