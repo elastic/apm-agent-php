fee168578ffad5571389cacb4d3700c5d0dd27b2
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-11-17 13:50:31 +0000

    [CI] Support PHP 8.rc

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -8,6 +8,7 @@ centos:centos7
 php:7.2-fpm
 php:7.3-fpm
 php:7.4-fpm
+php:8.0-rc-fpm
 ruby:2.7.1-alpine3.12
 "
 if [ -x "$(command -v docker)" ]; then
@@ -16,7 +17,7 @@ if [ -x "$(command -v docker)" ]; then
   (retry 2 docker pull "${di}") || echo "Error pulling ${di} Docker image, we continue"
   done
 
-  for version in 7.2 7.3 7.4
+  for version in 7.2 7.3 7.4 8.0-rc
   do
     PHP_VERSION=${version} make -f .ci/Makefile prepare || true
     DOCKERFILE=Dockerfile.alpine PHP_VERSION=${version} make -f .ci/Makefile prepare || true
