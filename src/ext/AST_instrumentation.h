0f1cdec2562e5179633bd060c8a4c8dbd64ea227
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2021-01-04 14:58:15 +0200

    Implemented auto-instrumentation for PHP (user-land) code

---
+++ src/ext/AST_instrumentation.h
@@ -0,0 +1,16 @@
+/*
+   +----------------------------------------------------------------------+
+   | Elastic APM agent for PHP                                            |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2020 Elasticsearch B.V.                                |
+   +----------------------------------------------------------------------+
+   | Elasticsearch B.V. licenses this file under the Apache 2.0 License.  |
+   | See the LICENSE file in the project root for more information.       |
+   +----------------------------------------------------------------------+
+ */
+
+#pragma once
+
+void astInstrumentationInit();
+
+void astInstrumentationShutdown();
