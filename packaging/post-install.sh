fcb5a7322391599f5e2d20effc86bcfa2f12dca3
Victor Martinez <victormartinezrubio@gmail.com>
2021-01-20 11:08:37 +0000

    Update packaging/post-install.sh

--- packaging/post-install.sh
+++ packaging/post-install.sh
@@ -192,9 +192,11 @@ function get_extension_file() {
 #### Function is_php_supported #################################################
 function is_php_supported() {
     PHP_MAJOR_MINOR=$(php_command -r 'echo PHP_MAJOR_VERSION;').$(php_command -r 'echo PHP_MINOR_VERSION;')
+    echo "Detected PHP version '${PHP_MAJOR_MINOR}'"
     if  [ "${PHP_MAJOR_MINOR}" == "7.2" ] || [ "${PHP_MAJOR_MINOR}" == "7.3" ] || [ "${PHP_MAJOR_MINOR}" == "7.4" ] ; then
         return 0
     else
+        echo 'Failed. The supported PHP versions are 7.2, 7.3 and 7.4.'
         return 1
     fi
 }
