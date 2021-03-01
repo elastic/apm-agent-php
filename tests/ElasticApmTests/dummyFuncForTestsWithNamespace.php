c9fae1c000084d1f1bdf2f6a72b4ad49ff763f60
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2021-03-01 12:10:41 +0200

    Added copyright headers to .php files

--- tests/ElasticApmTests/dummyFuncForTestsWithNamespace.php
+++ tests/ElasticApmTests/dummyFuncForTestsWithNamespace.php
@@ -1,5 +1,24 @@
 <?php
 
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 declare(strict_types=1);
 
 namespace ElasticApmTests;
@@ -8,7 +27,7 @@ use PHPUnit\Framework\TestCase;
 
 const DUMMY_FUNC_FOR_TESTS_WITH_NAMESPACE_CALLABLE_NAMESPACE = __NAMESPACE__;
 const DUMMY_FUNC_FOR_TESTS_WITH_NAMESPACE_CALLABLE_FILE_NAME = __FILE__;
-const DUMMY_FUNC_FOR_TESTS_WITH_NAMESPACE_CALLABLE_LINE_NUMBER = 21;
+const DUMMY_FUNC_FOR_TESTS_WITH_NAMESPACE_CALLABLE_LINE_NUMBER = 40;
 
 /**
  * @param callable $callable
