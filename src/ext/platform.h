4b66dcf9fbd4746617da20dd851b8a0c369304f2
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2021-04-12 12:30:36 +0300

    Include features.h only if it's gcc (GNU C compiler)

--- src/ext/platform.h
+++ src/ext/platform.h
@@ -20,9 +20,11 @@
 #pragma once
 
 #ifndef PHP_WIN32
-#   include <features.h>
-#   ifdef __GLIBC__
-#       define ELASTIC_APM_PLATFORM_HAS_BACKTRACE
+#   ifdef __GNUC__
+#       include <features.h>
+#       ifdef __GLIBC__
+#           define ELASTIC_APM_PLATFORM_HAS_BACKTRACE
+#       endif
 #   endif
 #endif
 
