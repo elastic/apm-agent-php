3455fbe11c46366f6101003f9c78fd3e406773dc
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-03-30 18:27:08 +0100

    ci: convert Test-Signed-Package matrix in a dynamic scripted parallel

---
+++ .ci/generate_stages_list_smoke.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+#
+# lifecycle-testing
+#
+for phpVersion in 7.2 7.3 7.4 8.0 8.1
+do
+    for linuxDistro in apk deb rpm tar
+    do
+        echo ${phpVersion},${linuxDistro},install-testing
+    done
+done
+
