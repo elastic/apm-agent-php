dfd27853bf4226cd379330524aaee7fb3763b35e
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-04-12 16:22:24 +0100

    Verify sha512 for the signed github assets

--- .ci/download-verify.sh
+++ .ci/download-verify.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 set -eo pipefail
+RELEASE_ID=${1}
+TARGET=${2}
+GITHUB=download
 
 echo 'download artifacts'
+mkdir $GITHUB || true
+cd $GITHUB
+## TODO: wait for https://github.com/cli/cli/pull/5442
+gh release download --release-id ${RELEASE_ID}
+cd ..
 
-echo 'verify artifacts'
+echo 'calculate the sha512'
+cat "$GITHUB/*.sha512" | sort > github.sha512
+cat "$TARGET/*.sha512" | sort > target.sha512
+
+echo 'verify sha512 are valid'
+diff github.sha512 target.sha512
+
