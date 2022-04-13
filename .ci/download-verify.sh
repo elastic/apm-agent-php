aa472095939ae044a0e02aafb2423821e2cb3366
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-04-13 13:32:25 +0100

    gh release download supports TAG from drafts

--- .ci/download-verify.sh
+++ .ci/download-verify.sh
@@ -8,13 +8,13 @@ echo 'download artifacts'
 mkdir $GITHUB || true
 cd $GITHUB
 ## TODO: wait for https://github.com/cli/cli/pull/5442
-gh release download --release-id ${RELEASE_ID}
+## gh release download --release-id ${RELEASE_ID}
+gh release download "$TAG_NAME"
 cd ..
 
 echo 'calculate the sha512'
-cat "$GITHUB/*.sha512" | sort > github.sha512
-cat "$TARGET/*.sha512" | sort > target.sha512
+sort "$GITHUB/*.sha512" > github.sha512
+sort "$TARGET/*.sha512" > target.sha512
 
 echo 'verify sha512 are valid'
 diff github.sha512 target.sha512
-
