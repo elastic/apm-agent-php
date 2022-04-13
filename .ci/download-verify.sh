60e3e5541981683193b563b2d34c0be384e9c7df
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-04-13 17:51:43 +0200

    ci: minor changes

--- .ci/download-verify.sh
+++ .ci/download-verify.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-set -eo pipefail
+set -exo pipefail
 RELEASE_ID=${1}
 TARGET=${2}
-GITHUB=download
+GITHUB=github
 
 echo 'download artifacts'
 mkdir $GITHUB || true
@@ -10,11 +10,12 @@ cd $GITHUB
 ## TODO: wait for https://github.com/cli/cli/pull/5442
 ## gh release download --release-id ${RELEASE_ID}
 gh release download "$TAG_NAME" --repo v1v/apm-agent-php
+ls -l
 cd ..
 
 echo 'calculate the sha512'
-sort "$GITHUB/*.sha512" > github.sha512
-sort "$TARGET/*.sha512" > target.sha512
+sort $GITHUB/*.sha512 > github.sha512
+sort $TARGET/*.sha512 > target.sha512
 
 echo 'verify sha512 are valid'
 diff github.sha512 target.sha512
