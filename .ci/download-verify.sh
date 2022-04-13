8bc34157a7a9407521f1cfce9358a523ff147d56
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-04-13 19:00:15 +0200

    ci: add signed / github sha512

--- .ci/download-verify.sh
+++ .ci/download-verify.sh
@@ -17,4 +17,10 @@ sort "$GITHUB/*.sha512" > github.sha512
 sort "$TARGET/*.sha512" > target.sha512
 
 echo 'verify sha512 are valid'
-diff github.sha512 target.sha512
+diff github.sha512 target.sha512 || exit 1
+
+echo 'debug: sha512 for the github artifacts'
+cat github.sha512
+
+echo 'debug: sha512 for the signed artifacts'
+cat target.sha512
