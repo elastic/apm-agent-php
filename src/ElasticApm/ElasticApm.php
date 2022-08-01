947edd1bd68f224c9f4c3f4b7e41538bc078ce7f
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2022-08-01 14:19:54 +0300

    Prepare for v1.6 release

--- src/ElasticApm/ElasticApm.php
+++ src/ElasticApm/ElasticApm.php
@@ -35,7 +35,7 @@ final class ElasticApm
 {
     use StaticClassTrait;
 
-    public const VERSION = '1.5.2';
+    public const VERSION = '1.6';
 
     /**
      * Begins a new transaction and sets it as the current transaction.
