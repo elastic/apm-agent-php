134fb46a2fb644877e96e7700fb1dc46b42d17fa
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2022-09-13 22:11:11 +0300

    Fixed "download and verify checksum"
    - Removed references to RELEASE_ID since it's not used (TAG_NAME is used instead)
    - Added missing part of verifying downloaded binaries checksums

---
+++ .ci/download_release_and_verify_checksum.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -xeo pipefail
+
+release_tag="${1}"
+original_artifacts_location="${2}"
+downloaded_artifacts_location="./github"
+
+echo "Downloading artifacts to ${downloaded_artifacts_location} ..."
+mkdir -p "${downloaded_artifacts_location}"
+pushd "${downloaded_artifacts_location}"
+gh release download "${release_tag}"
+popd
+
+ls -l "${original_artifacts_location}"
+ls -l "${downloaded_artifacts_location}"
+
+echo 'Verifying that downloaded artifacts pass the downloaded checksums...'
+sha512sum --check *.sha512
+
+sort "${original_artifacts_location}"/*.sha512 > original_artifacts.sha512
+sort "${downloaded_artifacts_location}"/*.sha512 > downloaded_artifacts.sha512
+cat original_artifacts.sha512
+cat downloaded_artifacts.sha512
+
+echo 'Verifying that original and downloaded artifacts have the same checksums...'
+diff original_artifacts.sha512 downloaded_artifacts.sha512 || exit 1
