6c19c83ea9460f858fb9b1ee972cd732f5225730
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2022-09-19 20:38:33 +0300

    Fixed wrongly shared local variable in generate_package_lifecycle_test_matrix.sh

--- .ci/generate_package_lifecycle_test_matrix.sh
+++ .ci/generate_package_lifecycle_test_matrix.sh
@@ -6,64 +6,81 @@ set -e
 # - Jenkinsfile (the list appears in Jenkinsfile more than once - search for "list of PHP versions")
 supportedPhpVersions=(7.2 7.3 7.4 8.0 8.1)
 
-function echoWithSuffixVariantsFromTestGroup () {
-    rowSoFar="$1"
-    for testGroup in no_ext_svc with_ext_svc
+function echoWithSuffixVariantsFromComponentTestsGroup () {
+    local rowSoFar="$1"
+    for componentTestsGroup in no_ext_svc with_ext_svc
     do
-        echo "${rowSoFar},${testGroup}"
+        echo "${rowSoFar},${componentTestsGroup}"
     done
 }
 
 function echoWithSuffixVariantsFromComponentTestsAppHostKind () {
-    rowSoFar="$1"
+    local rowSoFar="$1"
     for componentTestsAppHostKind in http cli
     do
-        echoWithSuffixVariantsFromTestGroup "${rowSoFar},${componentTestsAppHostKind}"
+        echoWithSuffixVariantsFromComponentTestsGroup "${rowSoFar},${componentTestsAppHostKind}"
     done
 }
 
-#
-# Lifecycle tests
-#
-testingType=lifecycle
-for phpVersion in "${supportedPhpVersions[@]}"
-do
-    for linuxPackageType in apk deb rpm tar
+function generateLifecycleRows () {
+    #
+    # Lifecycle tests
+    #
+    local testingType=lifecycle
+    for phpVersion in "${supportedPhpVersions[@]}"
     do
-        echoWithSuffixVariantsFromComponentTestsAppHostKind "${phpVersion},${linuxPackageType},${testingType}"
+        for linuxPackageType in apk deb rpm tar
+        do
+            echoWithSuffixVariantsFromComponentTestsAppHostKind "${phpVersion},${linuxPackageType},${testingType}"
+        done
     done
-done
+}
 
-#
-# Lifecycle tests for <app_server> (only for deb linuxDistro and http componentTestsAppHostKind)
-#
-linuxPackageType=deb
-componentTestsAppHostKind=http
-for phpVersion in "${supportedPhpVersions[@]}"
-do
-    for appServer in apache fpm
+function generateLifecycleOnProdServerRows () {
+    #
+    # Lifecycle tests for <app_server> (only for deb linuxDistro and http componentTestsAppHostKind)
+    #
+    local linuxPackageType=deb
+    local componentTestsAppHostKind=http
+    for phpVersion in "${supportedPhpVersions[@]}"
     do
-        testingType=lifecycle-${appServer}
-        echoWithSuffixVariantsFromTestGroup "${phpVersion},${linuxPackageType},${testingType},${componentTestsAppHostKind}"
+        for prodAppServer in apache fpm
+        do
+            local testingType=lifecycle-${prodAppServer}
+            echoWithSuffixVariantsFromComponentTestsGroup "${phpVersion},${linuxPackageType},${testingType},${componentTestsAppHostKind}"
+        done
     done
-done
+}
 
-#
-# PHP upgrade tests (only for rpm Linux distro)
-#
-phpVersion=7.2
-linuxPackageType=rpm
-testingType=php-upgrade
-echoWithSuffixVariantsFromComponentTestsAppHostKind "${phpVersion},${linuxPackageType},${testingType}"
+function generatePHPUpgradeRows () {
+    #
+    # PHP upgrade tests (only for rpm Linux distro)
+    #
+    local phpVersion=7.2
+    local linuxPackageType=rpm
+    local testingType=php-upgrade
+    echoWithSuffixVariantsFromComponentTestsAppHostKind "${phpVersion},${linuxPackageType},${testingType}"
+}
 
-#
-# Agent upgrade tests
-#
-testingType=agent-upgrade
-for phpVersion in 7.4 8.1
-do
-    for linuxPackageType in deb rpm
+function generateAgentUpgradeRows () {
+    #
+    # Agent upgrade tests
+    #
+    local testingType=agent-upgrade
+    for phpVersion in 7.4 8.1
     do
-        echoWithSuffixVariantsFromComponentTestsAppHostKind "${phpVersion},${linuxPackageType},${testingType}"
+        for linuxPackageType in deb rpm
+        do
+            echoWithSuffixVariantsFromComponentTestsAppHostKind "${phpVersion},${linuxPackageType},${testingType}"
+        done
     done
-done
+}
+
+function main () {
+    generateLifecycleRows
+    generateLifecycleOnProdServerRows
+    generatePHPUpgradeRows
+    generateAgentUpgradeRows
+}
+
+main
