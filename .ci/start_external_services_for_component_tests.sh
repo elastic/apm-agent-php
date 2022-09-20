ceb31109b3acac1aff1c04faf32186529c51e137
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2022-09-20 09:05:33 +0300

    Added --increase-timeout-exponentially to run_command_with_timeout_and_retries.sh

--- .ci/start_external_services_for_component_tests.sh
+++ .ci/start_external_services_for_component_tests.sh
@@ -14,10 +14,10 @@ if [ -z "${ELASTIC_APM_PHP_TESTS_EXTERNAL_SERVICES_START_CMD}" ] ; then
     export ELASTIC_APM_PHP_TESTS_EXTERNAL_SERVICES_START_CMD="docker-compose -f ${thisScriptDir}/docker-compose_external_services_for_component_tests.yml up -d"
 fi
 
-# Give longer timeout to allow docker to download images, etc.
-timeoutInMinutes=10
-timeoutInSeconds=$((timeoutInMinutes*60))
-
+run_command_with_timeout_and_retries_args=(--retry-on-error=yes)
+run_command_with_timeout_and_retries_args=(--max-tries=3 "${run_command_with_timeout_and_retries_args[@]}")
+run_command_with_timeout_and_retries_args=(--wait-time-before-retry=60 "${run_command_with_timeout_and_retries_args[@]}")
+run_command_with_timeout_and_retries_args=(--increase-wait-time-before-retry-exponentially=yes "${run_command_with_timeout_and_retries_args[@]}")
 # ELASTIC_APM_PHP_TESTS_EXTERNAL_SERVICES_START_CMD might contain multiple space separated arguments
 # shellcheck disable=SC2086
-"${thisScriptDir}/run_command_with_timeout_and_retries.sh" --timeout=${timeoutInSeconds} -- ${ELASTIC_APM_PHP_TESTS_EXTERNAL_SERVICES_START_CMD}
+"${thisScriptDir}/run_command_with_timeout_and_retries.sh" "${run_command_with_timeout_and_retries_args[@]}" -- ${ELASTIC_APM_PHP_TESTS_EXTERNAL_SERVICES_START_CMD}
