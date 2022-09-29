454a4c0e6cad18c568b86549259375c922232472
Ivan Tymkul <ivan_tymkul@epam.com>
2022-09-29 11:33:58 +0300

    Update symfony demo app. Containerization.

--- demos/Symfony/assets/js/highlight.js
+++
@@ -1,8 +0,0 @@
-import hljs from 'highlight.js/lib/core';
-import php from 'highlight.js/lib/languages/php';
-import twig from 'highlight.js/lib/languages/twig';
-
-hljs.registerLanguage('php', php);
-hljs.registerLanguage('twig', twig);
-
-hljs.highlightAll();
