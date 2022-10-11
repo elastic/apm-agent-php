cea9ab78cddd3302dcbfa49e028d7dfa599f50f1
Ivan Tymkul <ivan_tymkul@epam.com>
2022-10-11 11:35:16 +0300

    Init LAMINAS demo app. Containerization.

---
+++ demos/Laminas/module/Album/config/module.config.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Album;
+
+use Laminas\Router\Http\Segment;
+
+return [
+    'router' => [
+        'routes' => [
+            'album' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/album[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\AlbumController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'view_manager' => [
+        'template_path_stack' => [
+            'album' => __DIR__ . '/../view',
+        ],
+    ],
+];
