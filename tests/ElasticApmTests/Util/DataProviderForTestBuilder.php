6e0a20a5cadf7896511e6b03f97df308460c70ba
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2022-10-13 03:13:21 +0300

    Fixed syntax error

--- tests/ElasticApmTests/Util/DataProviderForTestBuilder.php
+++ tests/ElasticApmTests/Util/DataProviderForTestBuilder.php
@@ -184,8 +184,7 @@ final class DataProviderForTestBuilder
      */
     public function addBoolDimension(bool $onlyFirstValueCombinable): self
     {
-        $this->addDimension($onlyFirstValueCombinable, IterableUtilForTests::ALL_BOOL_VALUES);
-        return $this;
+        return $this->addDimension($onlyFirstValueCombinable, IterableUtilForTests::ALL_BOOL_VALUES);
     }
 
     /** @noinspection PhpUnused */
@@ -230,6 +229,17 @@ final class DataProviderForTestBuilder
     public function addBoolKeyedDimensionAllValuesCombinable(string $dimensionKey): self
     {
         return $this->addBoolKeyedDimension($dimensionKey, /* onlyFirstValueCombinable: */ false);
+    }
+
+    /**
+     * @param string $dimensionKey
+     * @param mixed $value
+     *
+     * @return $this
+     */
+    public function addSingleValueKeyedDimension(string $dimensionKey, $value): self
+    {
+        return $this->addKeyedDimension($dimensionKey, /* onlyFirstValueCombinable: */ true, [$value]);
     }
 
     /**
