1ec2647c0961e9a9dbb69d7814540039852e9055
Ivan Tymkul <ivan_tymkul@epam.com>
2022-12-21 16:41:04 +0200

    init yii2 demo app

---
+++ demos/Yii2/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql;dbname=database',
+    'username' => 'root',
+    'password' => 'pass',
+    'charset' => 'utf8',
+];
---
+++ demos/Yii2/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+chmod -R 777 assets/
+chmod -R 777 web/
+
+#php yii migrate
+
+#php-fpm
