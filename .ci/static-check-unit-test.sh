556dd986f08ad43a5c931b6f4466706acbe2319f
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2023-01-09 15:46:43 +0200

    Start syslog in .ci/static-check-unit-test.sh

--- .ci/static-check-unit-test.sh
+++ .ci/static-check-unit-test.sh
@@ -5,6 +5,56 @@ set -xe
 BUILD_FOLDER=/app/build
 mkdir -p ${BUILD_FOLDER}
 
+function ensureSyslogIsRunning () {
+    if ps -ef | grep -v 'grep' | grep -q 'syslogd' ; then
+        echo 'Syslog is already started.'
+        return
+    fi
+
+    if which rsyslogd; then
+        grep /var/log/messages /etc/rsyslog.conf
+        sed -i '/\/var\/log\/messages/s/messages/syslog/' /etc/rsyslog.conf
+        rsyslogd
+    else
+        if which syslogd; then
+            syslogd
+        else
+            echo 'syslog is not installed'
+            exit 1
+        fi
+    fi
+}
+
+function copySyslogFileAndPrintTheLastOne () {
+    ls -l /var/log/
+    local possibleSyslogFiles=(/var/log/messages /var/log/syslog)
+    for syslogFile in "${possibleSyslogFiles[@]}"
+    do
+        if [[ -f "${syslogFile}" ]]; then
+            local syslogCopyDir="${BUILD_FOLDER}/syslog"
+            mkdir -p "${syslogCopyDir}"
+            cp "${syslogFile}"* ${syslogCopyDir}
+            echo "syslog files (${syslogFile}*) copied to ${syslogCopyDir}"
+        fi
+    done
+
+    for syslogFile in "${possibleSyslogFiles[@]}"
+    do
+        if [[ -f "${syslogFile}" ]]; then
+            echo "Content of the most recent syslog file (${syslogFile}):"
+            cat "${syslogFile}"
+        fi
+    done
+}
+
+function onExit () {
+    copySyslogFileAndPrintTheLastOne
+}
+
+trap onExit EXIT
+
+ensureSyslogIsRunning
+
 ## This make runs PHPT
 # Disable agent for auxiliary PHP processes to reduce noise in logs
 export ELASTIC_APM_ENABLED=false
