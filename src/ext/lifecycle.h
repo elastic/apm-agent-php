c102896554311fc749319fc816b4fa0c21f6d9d8
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2023-02-22 17:09:33 +0200

    Fixed compilation error for PHP v.8.2

--- src/ext/lifecycle.h
+++ src/ext/lifecycle.h
@@ -22,9 +22,9 @@
 #include "ResultCode.h"
 
 
-void elasticApmModuleInit( int type, int moduleNumber );
+void elasticApmModuleInit( int moduleType, int moduleNumber );
 
-void elasticApmModuleShutdown( int type, int moduleNumber );
+void elasticApmModuleShutdown( int moduleType, int moduleNumber );
 
 void elasticApmRequestInit();
 
