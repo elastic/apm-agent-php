4239d65db442974d06059dd61d993554a2f6bae6
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2023-02-22 17:07:57 +0200

    Added PHP v8.2 to CI infrastructure

--- .ci/loop.groovy
+++ .ci/loop.groovy
@@ -71,7 +71,9 @@ pipeline {
             axes {
               axis {
                 name 'PHP_VERSION'
-                values '7.2', '7.3', '7.4', '8.0', '8.1'
+                // Make sure list of PHP versions supported by the Elastic APM PHP Agent is in sync.
+                // See the comment in .ci/shared.sh
+                values '7.2', '7.3', '7.4', '8.0', '8.1', '8.2'
               }
               axis {
                 name 'DOCKERFILE'
