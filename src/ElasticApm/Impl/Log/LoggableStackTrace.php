96c4c0657c4287cb2bcfd763e470f044f26af2fd
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2023-04-21 00:18:55 +0300

    Added maxNumberOfStackFrames as parameter to
    LoggableStackTrace::buildForCurrent instead having hardcoded

--- src/ElasticApm/Impl/Log/LoggableStackTrace.php
+++ src/ElasticApm/Impl/Log/LoggableStackTrace.php
@@ -36,12 +36,15 @@ final class LoggableStackTrace
 {
     public const STACK_TRACE_KEY = 'stacktrace';
 
+    public const MAX_NUMBER_OF_STACK_FRAMES = 100;
+
     /**
      * @param int $numberOfStackFramesToSkip
+     * @param int $maxNumberOfStackFrames
      *
      * @return ClassicFormatStackTraceFrame[]
      */
-    public static function buildForCurrent(int $numberOfStackFramesToSkip): array
+    public static function buildForCurrent(int $numberOfStackFramesToSkip, int $maxNumberOfStackFrames = self::MAX_NUMBER_OF_STACK_FRAMES): array
     {
         /**
          * @param string $key
@@ -58,7 +61,7 @@ final class LoggableStackTrace
             null /* <- loggerFactory */,
             $numberOfStackFramesToSkip + 1 /* <- offset */,
             DEBUG_BACKTRACE_IGNORE_ARGS /* <- options */,
-            100 /* limit */
+            $maxNumberOfStackFrames /* <- limit */
         );
         $result = [];
 
