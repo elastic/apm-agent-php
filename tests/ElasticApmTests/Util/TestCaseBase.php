1e65b0bb93a2c29c879398385efd0d6fa551777c
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2023-04-21 19:23:51 +0300

    Fixed issue with assertDirectoryDoesNotExist
    assertDirectoryDoesNotExist that was added only in PHPUnit 9 and it does not exist in PHPUnit 8.5 that we still use when testing under older PHP versions

--- tests/ElasticApmTests/Util/TestCaseBase.php
+++ tests/ElasticApmTests/Util/TestCaseBase.php
@@ -653,4 +653,23 @@ class TestCaseBase extends TestCase
             ++$dataSetIndex;
         }
     }
+
+    /** @inheritDoc */
+    public static function assertDirectoryDoesNotExist(string $directory, string $message = ''): void
+    {
+        /**
+         * Method assertDirectoryDoesNotExist was added in PHPUnit 9 as an alias for already existing assertDirectoryNotExists
+         * and assertDirectoryNotExists was deprecated.
+         * We still use PHPUnit 8.5 when testing under older PHP versions so we need a facade to work on both
+         *  - PHPUnit 8.5, where assertDirectoryDoesNotExist does not exist
+         *      and
+         *  - PHPUnit 9, where assertDirectoryNotExists is deprecated
+         */
+        if (method_exists(Assert::class, __FUNCTION__)) {
+            Assert::assertDirectoryDoesNotExist($directory, $message);
+        } else {
+            /** @noinspection PhpDeprecationInspection, PhpUnitDeprecatedCallsIn10VersionInspection */
+            Assert::assertDirectoryNotExists($directory, $message);
+        }
+    }
 }
