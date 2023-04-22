025cc0d9a53793f798ee87bfabfd2a963fecb789
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2023-04-22 11:11:55 +0300

    Removed unused imports

--- tests/ElasticApmTests/ComponentTests/WordPress/WordPressSpanExpectationsBuilder.php
+++ tests/ElasticApmTests/ComponentTests/WordPress/WordPressSpanExpectationsBuilder.php
@@ -23,7 +23,6 @@ declare(strict_types=1);
 
 namespace ElasticApmTests\ComponentTests\WordPress;
 
-use Elastic\Apm\Impl\AutoInstrument\WordPressAutoInstrumentation;
 use ElasticApmTests\Util\SpanExpectations;
 use ElasticApmTests\Util\SpanExpectationsBuilder;
 
