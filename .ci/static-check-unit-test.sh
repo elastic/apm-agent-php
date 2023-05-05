3c26cc5095d8eb53631e1300da054e3c7437f208
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2023-05-05 17:18:01 +0300

    Temporarily disable unit tests

--- .ci/static-check-unit-test.sh
+++ .ci/static-check-unit-test.sh
@@ -101,7 +101,10 @@ composer run-script static_check
 
 # Run unit tests
 phpUnitConfigFile=$(php ./tests/ElasticApmTests/Util/runSelectPhpUnitConfigFile.php --tests-type=unit)
-composer run-script -- run_unit_tests_custom_config -c "${phpUnitConfigFile}"
+# TODO: Sergey Kleyman: UNCOMMENT
+# composer run-script -- run_unit_tests_custom_config -c "${phpUnitConfigFile}"
+# TODO: Sergey Kleyman: REMOVE
+composer run-script -- run_unit_tests_custom_config -c "${phpUnitConfigFile}" --filter BreakdownMetricsTest
 ls -l ./build/unit-tests-phpunit-junit.xml
 
 # Generate junit output for phpstan
