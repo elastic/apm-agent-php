b6e347e987244fd5d1b548e04f97262b0ba5797e
Pawel Filipczak <pawel.filipczak@elastic.co>
2023-05-30 21:42:39 +0200

    separate tar package for debugsymbols

--- packaging/create-package.sh
+++ packaging/create-package.sh
@@ -34,6 +34,7 @@ fpm --input-type dir \
 		--before-remove=packaging/before-uninstall.sh \
 		--directories ${PHP_AGENT_DIR}/etc \
 		--config-files ${PHP_AGENT_DIR}/etc \
+		--exclude ${BUILD_EXT_DIR}/*.debug \
 		packaging/post-install.sh=${PHP_AGENT_DIR}/bin/post-install.sh \
 		build/elastic-apm.ini=${PHP_AGENT_DIR}/etc/ \
 		packaging/elastic-apm-custom-template.ini=${PHP_AGENT_DIR}/etc/elastic-apm-custom.ini \
@@ -52,6 +53,34 @@ rm "${OUTPUT}"/*.bck
 }
 
 
+function createDebugPackage () {
+
+fpm --input-type dir \
+		--output-type "${TYPE}" \
+		--name "${NAME}" \
+		--version "${VERSION}" \
+		--architecture all \
+		--url 'https://github.com/elastic/apm-agent-php' \
+		--maintainer 'APM Team <info@elastic.co>' \
+		--license 'ASL 2.0' \
+		--vendor 'Elasticsearch, Inc.' \
+		--description "PHP debug symbols agent for Elastic APM\nGit Commit: ${GIT_SHA}" \
+		--package "${OUTPUT}" \
+		--chdir /app ${FPM_FLAGS} \
+		--exclude ${BUILD_EXT_DIR}/*.so \
+		${BUILD_EXT_DIR}=${PHP_AGENT_DIR}
+
+## Create sha512
+BINARY=$(ls -1 "${OUTPUT}"/${NAME}*."${TYPE}")
+SHA=${BINARY}.sha512
+sha512sum "${BINARY}" > "${SHA}"
+sed -i.bck "s#${OUTPUT}/##g" "${SHA}"
+rm "${OUTPUT}"/*.bck
+
+}
+
+
+
 
 # create second tar for musl
 if [ "${TYPE}" = 'tar' ] ; then
@@ -60,9 +89,15 @@ if [ "${TYPE}" = 'tar' ] ; then
 	BUILD_EXT_DIR=build/ext/linux-x86-64/
 	createPackage
 
+	NAME="${NAME_BACKUP}-debugsymbols-linux-x86-64"
+	createDebugPackage
+
 	NAME="${NAME_BACKUP}-linuxmusl-x86-64"
 	BUILD_EXT_DIR=build/ext/linuxmusl-x86-64/
 	createPackage
+
+	NAME="${NAME_BACKUP}-debugsymbols-linuxmusl-x86-64"
+	createDebugPackage
 else
 	createPackage
 fi
