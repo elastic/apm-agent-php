51f9a7f33d6ab70b4a157a57cd91052443a74f7e
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-09-08 11:47:59 +0200

    remove unrequired bits and piecs

--- .ci/bump_version.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -xe
-
-sed -i.bck "s#\(VERSION = \).*;#\1'${VERSION}';#g" agent/php/ElasticApm/ElasticApm.php
-sed -i.bck "s#\(PHP_ELASTIC_APM_VERSION\).*#\1 \"${VERSION}\"#g" agent/native/ext/elastic_apm_version.h
-
-git add agent/php/ElasticApm/ElasticApm.php agent/native/ext/elastic_apm_version.h
