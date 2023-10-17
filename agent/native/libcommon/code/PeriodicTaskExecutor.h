e4de0b0bc5289010893762dbdaf4321c14a2fc72
Pawel Filipczak <pawel.filipczak@elastic.co>
2023-10-17 09:33:03 +0200

    fixed synchronization and task initialization

--- agent/native/libcommon/code/PeriodicTaskExecutor.h
+++ agent/native/libcommon/code/PeriodicTaskExecutor.h
@@ -5,9 +5,9 @@
 #include <atomic>
 #include <condition_variable>
 #include <functional>
-#include <list>
 #include <stop_token>
 #include <thread>
+#include <vector>
 
 namespace elasticapm::php {
 
@@ -25,16 +25,15 @@ public:
     using task_t = std::function<void(time_point_t)>;
     using worker_init_t = std::function<void()>;
 
-    PeriodicTaskExecutor(worker_init_t workerInit = {}) : workerInit_(std::move(workerInit)),  thread_(getThreadWorkerFunction()) {
+    PeriodicTaskExecutor(std::vector<task_t> periodicTasks, worker_init_t workerInit = {}) : periodicTasks_(periodicTasks), workerInit_(std::move(workerInit)),  thread_(getThreadWorkerFunction()) {
     }
 
     ~PeriodicTaskExecutor() {
-        {
-        std::lock_guard<std::mutex> lock(mutex_);
-        periodicTasks_.clear();
-        }
         shutdown();
-        thread_.join();
+
+        if (thread_.joinable()) {
+            thread_.join();
+        }
     }
 
     void work() {
@@ -44,31 +43,19 @@ public:
 
         std::unique_lock<std::mutex> lock(mutex_);
         while(working_) {
-            {
-                pauseCondition_.wait(lock, [this]() -> bool {
-                    return resumed_ || !working_;
-                });
-            }
+            pauseCondition_.wait(lock, [this]() -> bool {
+                return resumed_ || !working_;
+            });
 
             if (!working_) {
                 break;
             }
 
             lock.unlock();
-
-
             std::this_thread::sleep_for(sleepInterval_);
-            {
-                lock.lock();
-                for (auto const &task : periodicTasks_) {
-
-                    lock.unlock();
-                    task(std::chrono::time_point_cast<std::chrono::milliseconds>(clock_t::now()));
-                    lock.lock();
-                }
-                lock.unlock();
+            for (auto const &task : periodicTasks_) {
+                task(std::chrono::time_point_cast<std::chrono::milliseconds>(clock_t::now()));
             }
-
             lock.lock();
         }
     }
@@ -99,11 +86,6 @@ public:
         pauseCondition_.notify_all();
     }
 
-    void addPeriodicTask(task_t task) {
-        std::lock_guard<std::mutex> lock(mutex_);
-        periodicTasks_.emplace_back(std::move(task));
-    }
-
     void setInterval(std::chrono::milliseconds interval) {
         sleepInterval_ = interval;
     }
@@ -122,14 +104,14 @@ private:
 
 private:
 
-    worker_init_t workerInit_;
     std::chrono::milliseconds sleepInterval_ = std::chrono::milliseconds(20);
+    std::vector<task_t> periodicTasks_;
+    worker_init_t workerInit_;
     std::thread thread_;
-    std::list<task_t> periodicTasks_;
     std::mutex mutex_;
     std::condition_variable pauseCondition_;
-    std::atomic_bool working_ = true;
-    std::atomic_bool resumed_ = false;
+    bool working_ = true;
+    bool resumed_ = false;
 };
 
 
