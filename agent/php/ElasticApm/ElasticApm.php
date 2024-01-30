18da51cfe60d18a7c0ffda32d50569bff57745d2
Pawel Filipczak <pawel.filipczak@elastic.co>
2024-01-30 18:35:31 +0100

    Prepared for 1.13.0 release

--- agent/php/ElasticApm/ElasticApm.php
+++ agent/php/ElasticApm/ElasticApm.php
@@ -35,7 +35,7 @@ final class ElasticApm
 {
     use StaticClassTrait;
 
-    public const VERSION = '1.12.0';
+    public const VERSION = '1.13.0';
 
     /**
      * Begins a new transaction and sets it as the current transaction.
